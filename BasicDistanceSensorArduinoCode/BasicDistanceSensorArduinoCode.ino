04cb8aefa67a1286de5461bc67d777e4d971d40f
tomculling <tomcullingcomposer@gmail.com>
2026-06-20 17:19:52 +0100

    Add files via upload

---
+++ BasicDistanceSensorArduinoCode/BasicDistanceSensorArduinoCode.ino
@@ -0,0 +1,24 @@
+define TRIG_PIN 9
+#define ECHO_PIN 10
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+float getDistance() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH, 30000);
+  return (duration * 0.0343) / 2;
+}
+
+void loop() {
+  Serial.println(getDistance());
+  delay(50);
+}
